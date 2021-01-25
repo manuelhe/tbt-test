a9991c5b38becd807d1b7414dfa5e26a7d92e265
Manuel Herrera <334509+manuelhe@users.noreply.github.com>
2021-01-25 16:07:14 -0500

    feat: added client base code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="mainHeader">
+        <h1>TBT Test - Multiustep Form</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
